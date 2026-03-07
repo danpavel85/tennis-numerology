9fdd09f33c11d2ea1d50b60144c05ee68c819936
Dan Pavel <dan.pavel85@gmail.com>
2026-03-08 00:30:22 +0200

    fix render port

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import re
+import os
+
 
 app = Flask(__name__)
 
@@ -195,4 +197,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
