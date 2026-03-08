f4772122044b4f05e08b7b1254e39819a635ac67
Dan Pavel <dan.pavel85@gmail.com>
2026-03-08 18:44:00 +0200

    fix

--- app.py
+++ app.py
@@ -198,6 +198,6 @@ def profil():
 
 # ================= SERVER =================
 
-if __name__ == "__main__":
-    port=int(os.environ.get("PORT",10000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port=int(os.environ.get("PORT",10000))
+#     app.run(host="0.0.0.0", port=port)
